b8994f0f49fc859dfd3445f94f6f6587309fd654
Imam Husen Al <ihusen826@gmail.com>
2024-02-02 10:46:18 +0700

    Tugas 14 : Migration

---
+++ laravel-migration/database/migrations/2024_02_02_032232_create_film_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('film', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('ringkasan');
+            $table->string('tahun');
+            $table->string('poster');
+            $table->timestamps();
+
+            $table->unsignedBigInteger('genre_id');
+ 
+            $table->foreign('genre_id')->references('id')->on('genre');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('film');
+    }
+};
